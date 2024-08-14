0fe4d4771179b5466040693aad955ac73b977b86
Suhovius <alexey.suhoviy@gmail.com>
2024-08-15 02:45:04 +0300

    Use github source again

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "gke_cluster" {
-  # source         = "https://github.com/suhovius/tf-google-gke-cluster"
-  source         = "https://gitlab.com/suhovius/tf-google-gke-cluster"
+  source         = "https://github.com/suhovius/tf-google-gke-cluster"
+  # source         = "https://gitlab.com/suhovius/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
