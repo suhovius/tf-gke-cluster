75a3ffb10b2386ee5fe9bfd1527ce1be428360fe
Oleksii Sukhovii <alexey.suhoviy@gmail.com>
2024-08-15 02:07:14 +0000

    Add gcs terraform state storage. Fix git module source format

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 module "gke_cluster" {
-  source         = "https://github.com/suhovius/tf-google-gke-cluster"
-  # source         = "https://gitlab.com/suhovius/tf-google-gke-cluster"
+  source         = "git::https://github.com/suhovius/tf-google-gke-cluster.git"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "tf-gke-state-430719"
+    prefix = "terraform/state"
+  }
+}
+  
